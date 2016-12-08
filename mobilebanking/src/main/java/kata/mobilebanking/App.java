f98c7bab2b03ce645055155858b1c7762a51c381
Ben Wu <wubin28@gmail.com>
2016-12-08 23:57:19 +0800

    Hello Ben at 11:57pm

--- mobilebanking/src/main/java/kata/mobilebanking/App.java
+++ mobilebanking/src/main/java/kata/mobilebanking/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello Ben at 11:52pm");
+        System.out.println("Hello Ben at 11:57pm");
 
     }
 }
