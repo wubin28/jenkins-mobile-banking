776b8a5979e6f3fdc98463db606d8f725ce03e2a
Ben Wu <wubin28@gmail.com>
2016-12-09 23:12:07 +0800

    make a compilation error

--- mobilebanking/src/main/java/kata/mobilebanking/App.java
+++ mobilebanking/src/main/java/kata/mobilebanking/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello Ben at 09:58pm");
+        Systeddfm.out.println("Hello Ben at 09:58pm");
 
     }
 }
