9576eaf87896724a4f04fdf04357cbaa6d4f5343
Ben Wu <wubin28@gmail.com>
2016-12-11 17:33:32 +0800

    fix the compilation error and make a test failed

--- mobilebanking/src/main/java/kata/mobilebanking/App.java
+++ mobilebanking/src/main/java/kata/mobilebanking/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Syaastem.out.println("Hello Ben at 09:58pm");
+        System.out.println("Hello Ben at 09:58pm");
 
     }
 }
