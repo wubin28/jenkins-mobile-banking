f6b575da704544c2fdfb2b5c3a6e8070267435d9
Ben Wu <wubin28@gmail.com>
2017-02-24 14:56:53 +0800

    add a login test

---
+++ mobilebanking/src/test/java/kata/mobilebanking/LoginTest.java
@@ -0,0 +1,15 @@
+package kata.mobilebanking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by twer on 2/24/17.
+ */
+public class LoginTest {
+    @Test
+    public void should_be_successful_if_username_and_password_are_correct() {
+        assertTrue(true);
+    }
+}
