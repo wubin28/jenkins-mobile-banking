708a0d535fc033aabb593532c344589ec8f23434
Ben Wu <wubin28@gmail.com>
2017-03-11 17:43:00 +0800

    make unit test failed

--- mobilebanking/src/main/java/kata/mobilebanking/Login.java
+++ mobilebanking/src/main/java/kata/mobilebanking/Login.java
@@ -7,11 +7,9 @@ public class Login {
     public boolean loginWithUsernameAndPassword(String username, String password) {
         checkUsernameAndPassword(username, password);
         checkUsernameAndPassword(username, password);
-        checkUsernameAndPassword(username, password);
-        return true;
+        return false;
     }
 
     private void checkUsernameAndPassword(String username, String password) {
-        abc();
     }
 }
