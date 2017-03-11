c411d5ffa3dad62f382bf3421c1c7d7f734bb048
Ben Wu <wubin28@gmail.com>
2017-03-12 07:34:40 +0800

    update echo strings in Jenkinsfile

--- mobilebanking/src/main/java/kata/mobilebanking/Login.java
+++ mobilebanking/src/main/java/kata/mobilebanking/Login.java
@@ -6,7 +6,6 @@ package kata.mobilebanking;
 public class Login {
     public boolean loginWithUsernameAndPassword(String username, String password) {
         checkUsernameAndPassword(username, password);
-        checkUsernameAndPassword(username, password);
         return true;
     }
 
