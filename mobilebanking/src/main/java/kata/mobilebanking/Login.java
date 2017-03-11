6583c1989c3d6a71de6098114b5a96b4dad3a5e7
Ben Wu <wubin28@gmail.com>
2017-03-11 18:03:47 +0800

    make web ui test failed

--- mobilebanking/src/main/java/kata/mobilebanking/Login.java
+++ mobilebanking/src/main/java/kata/mobilebanking/Login.java
@@ -7,7 +7,7 @@ public class Login {
     public boolean loginWithUsernameAndPassword(String username, String password) {
         checkUsernameAndPassword(username, password);
         checkUsernameAndPassword(username, password);
-        return false;
+        return true;
     }
 
     private void checkUsernameAndPassword(String username, String password) {
