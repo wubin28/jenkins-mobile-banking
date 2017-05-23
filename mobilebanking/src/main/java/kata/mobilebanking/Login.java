e165ecf7ac2d82f3b7d500c082d9281ce0ab49c9
Ben Wu <wubin28@gmail.com>
2017-05-23 17:48:37 +0800

    call method checkUsernameAndPassword() twice

--- mobilebanking/src/main/java/kata/mobilebanking/Login.java
+++ mobilebanking/src/main/java/kata/mobilebanking/Login.java
@@ -6,6 +6,7 @@ package kata.mobilebanking;
 public class Login {
     public boolean loginWithUsernameAndPassword(String username, String password) {
         checkUsernameAndPassword(username, password);
+        checkUsernameAndPassword(username, password);
         return true;
     }
 
